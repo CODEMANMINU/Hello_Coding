cd227c7f33b7380f2a8454fa4d0ca7e60d05e136
MinuChoi <96402772+CODEMANMINU@users.noreply.github.com>
2022-01-17 12:40:35 +0900

    20220117

--- Hello_Coding/Assets/HelloCode.cs
+++ Hello_Coding/Assets/HelloCode.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloCode : MonoBehaviour { 
-
+public class HelloCode : MonoBehaviour
+{
+    float distance;
     // Start is called before the first frame update
-    void Start() {
-       Debug.Log("Hello World!");
+    void Start()
+    {
+        float distance = GetDistance(2, 2, 5, 6);
+        Debug.Log("(2,2)에서 (5,6)까지의 거리:" + distance);
+    }
+
+    public float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float width = x2 - x1;
+        float height = y2 - y1;
+
+        float distance = width * width + height * height;
+        distance = Mathf.Sqrt(distance);
+
+        return distance;
     }
 }
 
