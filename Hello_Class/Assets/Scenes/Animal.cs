1975765e945bf8da61b21adbbaca3482491cb0f1
MinuChoi <96402772+CODEMANMINU@users.noreply.github.com>
2022-01-20 12:48:14 +0900

    20220120
    dd

---
+++ Hello_Class/Assets/Scenes/Animal.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Animal { 
+
+    // Start is called before the first frame update
+
+    public string name;
+    public string sound;
+
+
+    public void PlaySound()
+    { 
+    Debug.Log(name+ ":" + sound);
+    }
+
+}
